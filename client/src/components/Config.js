302dd91a0627fc2f9694bc8657e52b874468c061
xxxxan2e <xxxxan2e@gmail.com>
2024-02-13 22:08:21 +0900

    오타수정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -8,4 +8,4 @@ export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3/';
 
 // 영화에 대한 이미지 값 넘겨받을 때
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
